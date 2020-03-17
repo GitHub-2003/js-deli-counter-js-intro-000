37dbf22410c519246b495dbc81e6a1c708efee4c
MJU <marie.ju2012@outlook.com>
2020-03-17 05:41:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+// accept two parameters: the current line of people, new person's name.
+// return a welcome message including the new person's position in line.
+function takeANumber(katzDeliLine, name) {
+  katzDeliLine.push(name)
+  return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`)
+}
+
+// accept the current line of people (katzDeliLine).
+// return the first person in line and then remove that individual from the line.
+// If there is nobody in line, display "... nobody waiting..." message
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    console.log("There is nobody waiting to be served!")
+  } else {
+    var name = katzDeliLine[0]
+    katzDeliLine.shift
+  }
+  return name
+}
+
+function currentLine(katzDeliLine) {
+
+}
